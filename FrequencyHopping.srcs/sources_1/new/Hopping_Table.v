ef5665ebe5bc9342b270241be50d0e874e409736
alvari <496199270@qq.com>
2025-10-01 13:44:03 +0800

    first commit

---
+++ FrequencyHopping.srcs/sources_1/new/Hopping_Table.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/09/30 15:32:56
+// Design Name: 
+// Module Name: Hopping_Table
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Hopping_Table(
+    input wire sequence,
+    output reg[28:0] K    
+    );
+    
+endmodule
